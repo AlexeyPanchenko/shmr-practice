9d0afdfc54615b4413a3f12bb04363aa90179b6b
AlexeyPanchenko <aolpanchenko@gmail.com>
2023-07-06 12:35:27 +0300

    fix plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
 }
+
+buildscript {
+    dependencies {
+        classpath(libs.coroutines)
+    }
+}
