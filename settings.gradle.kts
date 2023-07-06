082e240e4ad6b900181ca3de93d86b6140b3492e
AlexeyPanchenko <aolpanchenko@gmail.com>
2023-07-06 11:56:44 +0300

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.toml"))
+        }
+    }
+}
+rootProject.name = "practice"
+include(":app")
